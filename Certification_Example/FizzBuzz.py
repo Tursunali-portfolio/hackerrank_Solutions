91218fd25ea886804108aa98c70ac6ceed716790
Tursunali-portfolio <79641787+Tursunali-portfolio@users.noreply.github.com>
2021-04-09 22:35:25 +0500

    Create FizzBuzz.py

---
+++ Certification_Example/FizzBuzz.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# The function accepts INTEGER n as parameter.
+
+def fizzBuzz(n):
+    for i in range(1, n+1):
+        if i % 15 == 0:
+            print('FizzBuzz')
+        elif i % 5 == 0:
+            print('Buzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        else:
+            print(i)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    fizzBuzz(n)
